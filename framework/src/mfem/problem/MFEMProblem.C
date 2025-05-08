1faa1799ab6824550773cb4586f7234b95cc417c
Alexander Blair <alexander.blair@ukaea.uk>
2025-05-08 02:54:42 +0100

    Specify MFEMSubMeshTransfers in the Transfers input block

--- framework/src/mfem/problem/MFEMProblem.C
+++ framework/src/mfem/problem/MFEMProblem.C
@@ -492,33 +492,14 @@ MFEMProblem::addTransfer(const std::string & transfer_name,
                              const std::string & name,
                              InputParameters & parameters)
 {
-  if (parameters.getBase() == "MFEMSubMeshTransfer")
+  if (parameters.get<std::string>("_moose_base") == "MFEMSubMeshTransfer")
+  {
     FEProblemBase::addUserObject(transfer_name, name, parameters);
   } 
   else
-    FEProblemBase::addTransfer(transfer_name, name, parameters);
-}
-
-std::shared_ptr<mfem::ParGridFunction>
-MFEMProblem::getGridFunction(const std::string & name)
-{
-  return getUserObject<MFEMVariable>(name).getGridFunction();
-}
-
-void
-MFEMProblem::addInitialCondition(const std::string & ic_name,
-                                 const std::string & name,
-                                 InputParameters & parameters)
-{
-  FEProblemBase::addUserObject(ic_name, name, parameters);
-  getUserObject<MFEMInitialCondition>(name); // error check
-}
-
-std::string
-MFEMProblem::solverTypeString(const unsigned int libmesh_dbg_var(solver_sys_num))
-{
-  mooseAssert(solver_sys_num == 0, "No support for multi-system with MFEM right now");
-  return MooseUtils::prettyCppType(getProblemData().jacobian_solver.get());
+  {
+    FEProblemBase::addTransfer(transfer_name, name, parameters);    
+  }
 }
 
 #endif
