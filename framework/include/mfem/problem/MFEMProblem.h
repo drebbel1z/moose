loganharbour/platypus

Fixed assertion error when building functions

framework/include/mfem/problem/MFEMProblem.h
@@ -168,7 +168,7 @@ public:
   template <class T, class... Args>
   std::shared_ptr<T> makeScalarCoefficient(Args &&... args)
   {
-    return this->_problem_data._scalar_manager.make<T>(args...);
+    return this->_problem_data._scalar_factory.make<T>(args...);
   }
 
   /**
@@ -178,7 +178,7 @@ public:
   template <class T, class... Args>
   std::shared_ptr<T> makeVectorCoefficient(Args &&... args)
   {
-    return this->_problem_data._vector_manager.make<T>(args...);
+    return this->_problem_data._vector_factory.make<T>(args...);
   }
 
   /**
@@ -188,7 +188,7 @@ public:
   template <class T, class... Args>
   std::shared_ptr<T> makeMatrixCoefficient(Args &&... args)
   {
-    return this->_problem_data._matrix_manager.make<T>(args...);
+    return this->_problem_data._matrix_factory.make<T>(args...);
   }
 
   /**

drebbel1z/moose

framework/include/mfem/problem/MFEMProblem.h
@@ -175,7 +175,7 @@ public:
   template <class T, class... Args>
   std::shared_ptr<T> makeScalarCoefficient(Args &&... args)
   {
-    return this->_problem_data.scalar_manager.make<T>(args...);
+    return this->_problem_data.scalar_factory.make<T>(args...);
   }
 
   /**
@@ -185,7 +185,7 @@ public:
   template <class T, class... Args>
   std::shared_ptr<T> makeVectorCoefficient(Args &&... args)
   {
-    return this->_problem_data.vector_manager.make<T>(args...);
+    return this->_problem_data.vector_factory.make<T>(args...);
   }
 
   /**
@@ -195,7 +195,7 @@ public:
   template <class T, class... Args>
   std::shared_ptr<T> makeMatrixCoefficient(Args &&... args)
   {
-    return this->_problem_data.matrix_manager.make<T>(args...);
+    return this->_problem_data.matrix_factory.make<T>(args...);
   }
 
   /**