loganharbour/platypus

Rename FunctionBCs to FunctorBCs

framework/src/mfem/bcs/MFEMScalarFunctorDirichletBC.C
@@ -1,9 +1,9 @@
-#include "MFEMScalarFunctionDirichletBC.h"
+#include "MFEMScalarFunctorDirichletBC.h"
 
-registerMooseObject("PlatypusApp", MFEMScalarFunctionDirichletBC);
+registerMooseObject("PlatypusApp", MFEMScalarFunctorDirichletBC);
 
 InputParameters
-MFEMScalarFunctionDirichletBC::validParams()
+MFEMScalarFunctorDirichletBC::validParams()
 {
   InputParameters params = MFEMEssentialBC::validParams();
   params.addRequiredParam<std::string>(
@@ -11,7 +11,7 @@ MFEMScalarFunctionDirichletBC::validParams()
   return params;
 }
 
-MFEMScalarFunctionDirichletBC::MFEMScalarFunctionDirichletBC(const InputParameters & parameters)
+MFEMScalarFunctorDirichletBC::MFEMScalarFunctorDirichletBC(const InputParameters & parameters)
   : MFEMEssentialBC(parameters),
     _coef_name(getParam<std::string>("coefficient")),
     _coef(getMFEMProblem().getProperties().getScalarProperty(_coef_name))
@@ -19,7 +19,7 @@ MFEMScalarFunctionDirichletBC::MFEMScalarFunctionDirichletBC(const InputParamete
 }
 
 void
-MFEMScalarFunctionDirichletBC::ApplyBC(mfem::GridFunction & gridfunc, mfem::Mesh * mesh_)
+MFEMScalarFunctorDirichletBC::ApplyBC(mfem::GridFunction & gridfunc, mfem::Mesh * mesh_)
 {
   mfem::Array<int> ess_bdrs(mesh_->bdr_attributes.Max());
   ess_bdrs = getBoundaries();

framework/src/mfem/bcs/MFEMVectorFunctorNormalIntegratedBC.C
@@ -1,18 +1,19 @@
-#include "MFEMVectorFunctionBoundaryIntegratedBC.h"
+#include "MFEMVectorFunctorNormalIntegratedBC.h"
 
-registerMooseObject("PlatypusApp", MFEMVectorFunctionBoundaryIntegratedBC);
+registerMooseObject("PlatypusApp", MFEMVectorFunctorNormalIntegratedBC);
 
 InputParameters
-MFEMVectorFunctionBoundaryIntegratedBC::validParams()
+MFEMVectorFunctorNormalIntegratedBC::validParams()
 {
   InputParameters params = MFEMIntegratedBC::validParams();
-  params.addRequiredParam<std::string>("vector_coefficient",
-                                       "Vector coefficient used in the boundary integrator.");
+  params.addRequiredParam<std::string>(
+      "vector_coefficient",
+      "The vector function whose normal component will be used in the integrated BC");
   return params;
 }
 
 // TODO: Currently assumes the vector function coefficient is 3D
-MFEMVectorFunctionBoundaryIntegratedBC::MFEMVectorFunctionBoundaryIntegratedBC(
+MFEMVectorFunctorNormalIntegratedBC::MFEMVectorFunctorNormalIntegratedBC(
     const InputParameters & parameters)
   : MFEMIntegratedBC(parameters),
     _vec_coef(getMFEMProblem().getProperties().getVectorProperty(
@@ -23,14 +24,14 @@ MFEMVectorFunctionBoundaryIntegratedBC::MFEMVectorFunctionBoundaryIntegratedBC(
 // Create a new MFEM integrator to apply to the RHS of the weak form. Ownership managed by the
 // caller.
 mfem::LinearFormIntegrator *
-MFEMVectorFunctionBoundaryIntegratedBC::createLFIntegrator()
+MFEMVectorFunctorNormalIntegratedBC::createLFIntegrator()
 {
-  return new mfem::VectorBoundaryLFIntegrator(_vec_coef);
+  return new mfem::BoundaryNormalLFIntegrator(_vec_coef);
 }
 
 // Create a new MFEM integrator to apply to LHS of the weak form. Ownership managed by the caller.
 mfem::BilinearFormIntegrator *
-MFEMVectorFunctionBoundaryIntegratedBC::createBFIntegrator()
+MFEMVectorFunctorNormalIntegratedBC::createBFIntegrator()
 {
   return nullptr;
 }

drebbel1z/moose

framework/src/mfem/bcs/MFEMScalarFunctorDirichletBC.C
@@ -1,12 +1,11 @@
 #ifdef MFEM_ENABLED
 
-#include "MFEMScalarFunctionDirichletBC.h"
-#include "MFEMProblem.h"
+#include "MFEMScalarFunctorDirichletBC.h"
 
-registerMooseObject("MooseApp", MFEMScalarFunctionDirichletBC);
+registerMooseObject("MooseApp", MFEMScalarFunctorDirichletBC);
 
 InputParameters
-MFEMScalarFunctionDirichletBC::validParams()
+MFEMScalarFunctorDirichletBC::validParams()
 {
   InputParameters params = MFEMEssentialBC::validParams();
   params.addRequiredParam<std::string>(
@@ -14,7 +13,7 @@ MFEMScalarFunctionDirichletBC::validParams()
   return params;
 }
 
-MFEMScalarFunctionDirichletBC::MFEMScalarFunctionDirichletBC(const InputParameters & parameters)
+MFEMScalarFunctorDirichletBC::MFEMScalarFunctorDirichletBC(const InputParameters & parameters)
   : MFEMEssentialBC(parameters),
     _coef_name(getParam<std::string>("coefficient")),
     _coef(getMFEMProblem().getProperties().getScalarProperty(_coef_name))
@@ -22,7 +21,7 @@ MFEMScalarFunctionDirichletBC::MFEMScalarFunctionDirichletBC(const InputParamete
 }
 
 void
-MFEMScalarFunctionDirichletBC::ApplyBC(mfem::GridFunction & gridfunc, mfem::Mesh & mesh)
+MFEMScalarFunctorDirichletBC::ApplyBC(mfem::GridFunction & gridfunc, mfem::Mesh & mesh)
 {
   mfem::Array<int> ess_bdrs(mesh.bdr_attributes.Max());
   ess_bdrs = getBoundaries();

framework/src/mfem/bcs/MFEMVectorFunctorNormalIntegratedBC.C
@@ -1,21 +1,21 @@
 #ifdef MFEM_ENABLED
 
-#include "MFEMVectorFunctionBoundaryIntegratedBC.h"
-#include "MFEMProblem.h"
+#include "MFEMVectorFunctorNormalIntegratedBC.h"
 
-registerMooseObject("MooseApp", MFEMVectorFunctionBoundaryIntegratedBC);
+registerMooseObject("MooseApp", MFEMVectorFunctorNormalIntegratedBC);
 
 InputParameters
-MFEMVectorFunctionBoundaryIntegratedBC::validParams()
+MFEMVectorFunctorNormalIntegratedBC::validParams()
 {
   InputParameters params = MFEMIntegratedBC::validParams();
-  params.addRequiredParam<std::string>("vector_coefficient",
-                                       "Vector coefficient used in the boundary integrator.");
+  params.addRequiredParam<std::string>(
+      "vector_coefficient",
+      "The vector function whose normal component will be used in the integrated BC");
   return params;
 }
 
 // TODO: Currently assumes the vector function coefficient is 3D
-MFEMVectorFunctionBoundaryIntegratedBC::MFEMVectorFunctionBoundaryIntegratedBC(
+MFEMVectorFunctorNormalIntegratedBC::MFEMVectorFunctorNormalIntegratedBC(
     const InputParameters & parameters)
   : MFEMIntegratedBC(parameters),
     _vec_coef(getMFEMProblem().getProperties().getVectorProperty(
@@ -26,14 +26,14 @@ MFEMVectorFunctionBoundaryIntegratedBC::MFEMVectorFunctionBoundaryIntegratedBC(
 // Create a new MFEM integrator to apply to the RHS of the weak form. Ownership managed by the
 // caller.
 mfem::LinearFormIntegrator *
-MFEMVectorFunctionBoundaryIntegratedBC::createLFIntegrator()
+MFEMVectorFunctorNormalIntegratedBC::createLFIntegrator()
 {
-  return new mfem::VectorBoundaryLFIntegrator(_vec_coef);
+  return new mfem::BoundaryNormalLFIntegrator(_vec_coef);
 }
 
 // Create a new MFEM integrator to apply to LHS of the weak form. Ownership managed by the caller.
 mfem::BilinearFormIntegrator *
-MFEMVectorFunctionBoundaryIntegratedBC::createBFIntegrator()
+MFEMVectorFunctorNormalIntegratedBC::createBFIntegrator()
 {
   return nullptr;
 }