42dfe291b3b51cbf341c98ef5d23baf8bf40b244
drebbel1z <drebbel1z@gmail.com>
2025-07-29 08:39:55 -0600

    mobo getting there

--- modules/stochastic_tools/src/reporters/GenericActiveLearner.C
+++ modules/stochastic_tools/src/reporters/GenericActiveLearner.C
@@ -52,14 +52,9 @@ GenericActiveLearner::GenericActiveLearner(const InputParameters & parameters)
   : GeneralReporter(parameters),
     ParallelAcquisitionInterface(parameters),
     SurrogateModelInterface(this),
-    _output_value(getReporterValue<std::vector<std::vector<Real>>>("output_value",
-                                                                   REPORTER_MODE_DISTRIBUTED)),
-    _output_comm(declareValue<std::vector<std::vector<Real>>>("outputs_required")),
     _sampler(getSampler("sampler")),
     _al_sampler(dynamic_cast<const GenericActiveLearningSampler *>(&_sampler)),
     _sorted_indices(declareValue<std::vector<unsigned int>>("sorted_indices")),
-    _al_gp(getUserObject<std::vector<ActiveLearningGaussianProcess>>("al_gp")),
-    _gp_eval(getSurrogateModel<std::vector<GaussianProcessSurrogate>>("gp_evaluator")),
     _acquisition_obj(getParallelAcquisitionFunctionByName(getParam<UserObjectName>("acquisition"))),
     _acquisition_value(declareValue<std::vector<Real>>("acquisition_function")),
     _convergence_value(declareValue<Real>("convergence_value")),
@@ -69,6 +64,18 @@ GenericActiveLearner::GenericActiveLearner(const InputParameters & parameters)
     _local_comm(_sampler.getLocalComm()),
     _num_objs(getParam<int>("num_objs"))
 {
+  std::vector<std::string> output_value_names = getParam<std::vector<std::string>>("output_value");
+  std::vector<std::string> al_gp_names = getParam<std::vector<std::string>>("al_gp");
+  std::vector<std::string> gp_evaluator_names = getParam<std::vector<std::string>>("gp_evaluator");
+  for (size_t i = 0; i < _num_objs; i++)
+  {
+    _output_value.push_back(
+        getReporterValue<std::vector<Real>>(output_value_names[i], REPORTER_MODE_DISTRIBUTED));
+    _al_gp.push_back(getUserObject<ActiveLearningGaussianProcess>(al_gp_names[i]));
+    _gp_eval.push_back(getSurrogateModel<GaussianProcessSurrogate>(gp_evaluator_names[i]));
+  }
+
+  _output_comm = declareValue<std::vector<std::vector<Real>>>("outputs_required");
 }
 
 void
