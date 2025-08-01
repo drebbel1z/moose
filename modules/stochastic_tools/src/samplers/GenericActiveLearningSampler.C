d26643afd8ec184f953c8235f65be9f53d7166a1
drebbel1z <drebbel1z@gmail.com>
2025-08-01 12:52:53 -0600

    ...

--- modules/stochastic_tools/src/samplers/GenericActiveLearningSampler.C
+++ modules/stochastic_tools/src/samplers/GenericActiveLearningSampler.C
@@ -109,9 +109,8 @@ GenericActiveLearningSampler::sampleSetUp(const Sampler::SampleMode /*mode*/)
   Else, generate the samples informed by the GP from the reporter "sorted_indices" */
   for (dof_id_type i = 0; i < _num_parallel_proposals; ++i)
   {
-    if (_t_step <= 1)
+    if (_t_step < 1)
     {
-
       for (unsigned int j = 0; j < _distributions.size(); ++j)
         _sample_vector[j] = _distributions[j]->quantile(_probabilities[j][i]);
       _new_samples[i] = _sample_vector;
