403f7436ff81b86c1c72167ab1e0244b65507bcd
drebbel1z <drebbel1z@gmail.com>
2025-08-27 15:13:07 -0600

    moving csv reader to constructor

--- modules/stochastic_tools/include/reporters/GenericActiveLearner.h
+++ modules/stochastic_tools/include/reporters/GenericActiveLearner.h
@@ -158,5 +158,7 @@ protected:
   std::vector<Real> _generic;
 
   /// The GP outputs from the current iteration before re-training (to evaluate convergence)
-  std::vector<Real> _eval_outputs_current;
+  std::vector<std::vector<Real>> _eval_outputs_current;
+
+  std::vector<std::vector<Real>> _csv_data;
 };
